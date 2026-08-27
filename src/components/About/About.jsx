6dc166a706651322e547e3fd56a36f5d1e4cf322
ThunkableX CREATORS Technology <thunkablecreators@gmail.com>
2026-08-27 14:56:59 +0300

    Added the Hero.jsx component to the project

---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+
+export default function About () {
+    return (
+        <div>
+            <h1>About | Mark Maxwell Mathan</h1>
+            <img src="" alt="mark maxwell mathan" />
+            <div>
+                <p>Mark Maxwell Mathan is a MERN Stack web developer</p>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Hero/Home.jsx
@@ -0,0 +1,14 @@
+
+export default function Home () {
+    return (
+        <div>
+            <h1 className="Header">Home | My Portfolio</h1>
+            <div>
+                <h2>Mark Maxwell Mathan</h2>
+                <aside>Hey, I am a frontend MERN Stack Web Developer</aside>
+                <img src="" alt="Mark Maxwell Mathan" />
+                <a href="Projects.jsx">View Projects</a>
+            </div>
+        </div>
+    )
+}
