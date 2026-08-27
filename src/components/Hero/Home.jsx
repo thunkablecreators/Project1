3910602393aa3029f2540104bcf12c9aa6501f99
ThunkableX CREATORS Technology <thunkablecreators@gmail.com>
2026-08-27 15:11:37 +0300

    Added some basic stylings to Home.jsx

--- src/components/Hero/Home.jsx
+++ src/components/Hero/Home.jsx
@@ -1,12 +1,20 @@
 
 export default function Home () {
+    const style = {
+        "borderRadius": "12px",
+        "border": "1px solid black",
+        "padding": "15px",
+        "backgroundColor": "#09edcf",
+        "color":"#f56505",
+        "fontFamily": "sanSerif"
+    }
     return (
         <div>
-            <h1 className="Header">Home | My Portfolio</h1>
+            <h1 style={style}>Home | My Portfolio</h1>
             <div>
                 <h2>Mark Maxwell Mathan</h2>
                 <aside>Hey, I am a frontend MERN Stack Web Developer</aside>
-                <img src="" alt="Mark Maxwell Mathan" />
+                <img src="/images/Mark.jpg" alt="Mark Maxwell Mathan" />
                 <a href="Projects.jsx">View Projects</a>
             </div>
         </div>
